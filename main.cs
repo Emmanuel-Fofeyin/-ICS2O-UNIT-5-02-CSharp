482ed25842798da40fd37d74954e6f00a1600c9d
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-06 23:33:01 +0000

    added code to file

--- main.cs
+++ main.cs
@@ -1,7 +1,28 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int integer;
+
+        Console.WriteLine("Please enter an integer");
+        Console.WriteLine("");
+
+        Console.Write("integer: ");
+        integer = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+
+        if (integer >= 0)
+        {
+        Console.WriteLine("Your number is positive");
+        } 
+        else 
+        {
+        Console.WriteLine("Your number is negative");
+        }
+
+        Console.WriteLine("\nDone.");
+    }
 }
